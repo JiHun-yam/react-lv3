982813d0769e0cbe149ad59408bd88c7809f9c11
음지훈 <jhmc5268@naver.com>
2023-02-16 16:10:25 +0900

    feat: 버튼 기능 완료

--- src/componets/Button/ButtonArea.jsx
+++ src/componets/Button/ButtonArea.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 
 const ButArea = styled.div`
     width: 100%;
     height: 25%;
-    border: 1px solid blue;
+    
     padding-left: 20px;
     gap: 20px;
 `
@@ -18,15 +18,35 @@ const ButtonGround = styled.div`
 `;
 // 버튼 초기값 
 const Btn = styled.button`
-    height: 50px;
+    height: 3rem;
+    width: 5rem;
     border-radius: 15px;
     color: ${(props) => props.Color};
-    border: 1px solid ${(props) => props.borderColor};
     background: ${(props) => props.back};
-    :hover {
+    border: none;
+    border: 1px solid ${(props) => props.borderColor};
+    &:hover {
     cursor: pointer;
      }
-
+    &:active{
+        opacity: 0.6;
+    }   
+    ${(props) =>
+        props.LgBtn
+        &&
+        css`
+      width: 10rem;
+      height: 3.8rem;
+    `};
+    ${(props) =>
+        props.mgBtn
+        &&
+        css`
+        height: 3.4rem;
+      width: 7rem;
+      border: none;
+    `};
+   
 
   
 `;
@@ -35,6 +55,14 @@ function ButtonArea() {
     let ButtonLabel = ['Cool Button', 'Hot Button'];
 
 
+    const BtnBackcolor = (label) => {
+        switch (label) {
+            case 'Cool Button':
+                return '#8EA7E9'
+            default:
+                return '#F48484';
+        }
+    }
 
     const onBtnClick = () => {
         alert('버튼을 만들어보세요')
@@ -54,21 +82,25 @@ function ButtonArea() {
                     return (
                         <ButtonGround key={i}>
                             <Btn
-                                back='495579'
-                                Color="495579"
-                                onClick={i == 0 ? onBtnClick2 : onBtnClick}
+                                back='white'
+                                borderColor={BtnBackcolor(v)}
+                                Color={i === 0 ? 'black' : 'red'}
+                                onClick={i == 0 ? onBtnClick : onBtnClick2}
+                                LgBtn
                             >
                                 {i === 0 ? `${v} 🥶` : `${v} 🥵`}
                             </Btn>
 
                             <Btn
-                                Color="495579"
-                                borderColor="red"
-                                mgBtn>Medium</Btn>
+                                // 케이스별 백그라운드색깔 넣기 
+                                back={BtnBackcolor(v)}
+                                borderColor={BtnBackcolor(v)}
+                                mgBtn>
+                                Medium</Btn>
 
                             <Btn
-                                Color="495579"
-                                borderColor="yellow"
+                                back={BtnBackcolor(v)}
+                                borderColor={BtnBackcolor(v)}
                                 smBtn>Smail</Btn>
 
                         </ButtonGround>
