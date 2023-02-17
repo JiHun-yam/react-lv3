dbe747f88493cd73ce6d0a9e2eab18e0b2f295ff
음지훈 <jhmc5268@naver.com>
2023-02-17 09:27:44 +0900

    feat: 모달 2 기능 완성

--- src/componets/Modal/ModalArea.jsx
+++ src/componets/Modal/ModalArea.jsx
@@ -1,10 +1,12 @@
-import React, { useState } from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 import styled from "styled-components";
 import { Btn } from '../Button/ButtonArea';
 
 
 function ModalArea() {
+    // 첫번째 모달 
     const [modal, setModal] = useState('none');
+    // 두번째 모달
     const [modal2, setModal2] = useState('none');
 
     const OpenModal = (e) => (
@@ -14,6 +16,18 @@ function ModalArea() {
     const closeModal = (e) => (
         e.target.name === 'close' ? setModal('none') : setModal2('none')
     );
+
+    // 2번 모달창 outside 클릭시 close
+    const outside = useRef();
+
+    useEffect(() => {
+        const outsideClick = (e) => {
+            if (!outside.current.contains(e.target)) {
+                setModal2('none');
+            }
+        };
+        document.addEventListener('mousedown', outsideClick);
+    });
     return (
         <ModArea>
             <h2>Modal</h2>
@@ -21,12 +35,12 @@ function ModalArea() {
                 name='one'
                 onClick={OpenModal}
                 inModal
-                back={'#8EA7E9'}>안요소클릭모달</Btn>
+                back={'#8EA7E9'}>Modal</Btn>
             <Btn
                 name='two'
                 onClick={OpenModal}
                 back={'#F48484'}
-                outModal>밖요소클릭모달</Btn>
+                outModal>Modal</Btn>
 
 
             <Modaloutside modal={modal}>
@@ -38,6 +52,18 @@ function ModalArea() {
                         close
                     </button>
                     <button>ok</button>
+
+                </ModalInside>
+            </Modaloutside>
+
+            <Modaloutside modal={modal2}>
+                <ModalInside modal={modal2} ref={outside}>
+                    <button style={{
+                        zIndex: '11111',
+                    }}
+                        onClick={closeModal} name={'close'}>
+                        밖요소 클릭시 모달 사라짐
+                    </button>
 
                 </ModalInside>
             </Modaloutside>
@@ -85,8 +111,8 @@ const ModalInside = styled.div`
     
     > button {
         margin-right: 10px;
-        width: 100px;
-        height: 30px;
+        width: 10rem;
+        height: 5rem;
         border-radius: 20px;
         border: none;
         cursor: pointer;
